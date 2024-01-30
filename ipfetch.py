fd1ef8c6db54927a0478c42f690a0188c760491c
Sascha Mester <mettbroetchen43@gmail.com>
2024-01-30 11:02:48 +0100

    Code Refactoring

--- ipfetch.py
+++ ipfetch.py
@@ -1,17 +1,20 @@
 import requests
-import json
+from os import system, name
+from version import getVersion as gv
 
-from version import getVersion as gv 
-
-# URL for IPv4 API (4.myip.is)
+# URLs for IPv4 and IPv6 APIs
 ipv4_url = 'https://4.myip.is/'
-
-# URL for IPv6 API (6.myip.is)
 ipv6_url = 'https://6.myip.is/'
 
-# Method for calling and parsing JSON-data of given URL
+def clear_screen():
+    '''Clears the screen.'''
+    if name == "nt":
+        _ = system("cls")
+    elif name == "posix":
+        _ = system("clear")
+
 def get_ip_info(url):
-    """Calls the specified URL and parses the JSON data.
+    '''Calls the specified URL and parses the JSON data.
 
     Args:
         url (str): The URL to call.
@@ -21,54 +24,51 @@ def get_ip_info(url):
 
     Raises:
         requests.RequestException: If an error occurs during the request.
-    """
+    '''
     try:
         response = requests.get(url)
-        if response.status_code == 200:
-            parsed_json = response.json()
-            return parsed_json
-        else:
-            print(f"Error while getting Data. Statuscode: {response.status_code}")
+        response.raise_for_status()  # Raises an HTTPError for bad responses
+        return response.json()
     except requests.RequestException as e:
         print(f"Error on request: {e}")
         print("Please check your internet connection.")
 
+def print_ip_info(ip_data, protocol):
+    '''Prints IP information.'''
+    nl(1)
+    print(f"IPv{protocol}")
+    print("=" * 4)
+    nl(1)
+    print(f"IP-Address   : {ip_data['ip']}")
+    print(f"Host         : {ip_data['host']}")
+    print(f"Timestamp    : {ip_data['timestamp']}")
+
 def nl(count):
-    '''Prints specified number of new lines.
-       You should use Integer Values.'''
+    '''Prints specified number of new lines.'''
     if count > 0:
         try:
-            for i in range(count):
+            for _ in range(count):
                 print()
         except TypeError as e:
             print(f"{count} is not a valid value! Use integers!")
             print(f"Error: {e}")
             raise
-    else:
-        pass
-    
 
-#Welcome Message
-print (f"Welcome to ipfetch, Version {gv()}")
-nl(1)
+def main():
+    clear_screen()
+    # Welcome Message
+    print(f"Welcome to ipfetch, Version {gv()}")
+    nl(1)
 
-# Getting and parsing Data of IPv4-API
-ipv4_data = get_ip_info(ipv4_url)
-if ipv4_data:
-    print("IPv4")
-    print("====")
-    nl(1)
-    print(f"IP-Adress  : {ipv4_data['ip']}")
-    print(f"Host       : {ipv4_data['host']}")
-    print(f"Timestamp  : {ipv4_data['timestamp']}")
+    # Getting and parsing Data of IPv4-API
+    ipv4_data = get_ip_info(ipv4_url)
+    if ipv4_data:
+        print_ip_info(ipv4_data, 4)
 
-# Getting and parsing Data of IPv6-API
-ipv6_data = get_ip_info(ipv6_url)
-if ipv6_data:
-    nl(1)
-    print("IPv6")
-    print("====")
-    nl(1)
-    print(f"IP-Adress  : {ipv6_data['ip']}")
-    print(f"Host       : {ipv6_data['host']}")
-    print(f"Timestamp  : {ipv6_data['timestamp']}")
+    # Getting and parsing Data of IPv6-API
+    ipv6_data = get_ip_info(ipv6_url)
+    if ipv6_data:
+        print_ip_info(ipv6_data, 6)
+
+if __name__ == "__main__":
+    main()
